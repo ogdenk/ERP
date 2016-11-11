4743868bde006583964b3d603dc25fc50c1e157e
Kent Ogden <ogdenk@upstate.edu>
2016-11-11 09:26:57 -0500

    Added deep copy of ITK image to facilitate processing of multiple versions of image data.

---
+++ DeepCopy.h
@@ -0,0 +1,21 @@
+#include "itkImage.h"
+#include "itkImageRegionIterator.h"
+
+template<typename TImage>
+void DeepCopy(typename TImage::Pointer input, typename TImage::Pointer output)
+{
+  output->SetRegions(input->GetLargestPossibleRegion());
+  output->SetSpacing(input->GetSpacing());
+  output->SetOrigin(input->GetOrigin());
+  output->Allocate();
+ 
+  itk::ImageRegionConstIterator<TImage> inputIterator(input, input->GetLargestPossibleRegion());
+  itk::ImageRegionIterator<TImage> outputIterator(output, output->GetLargestPossibleRegion());
+ 
+  while(!inputIterator.IsAtEnd())
+    {
+    outputIterator.Set(inputIterator.Get());
+    ++inputIterator;
+    ++outputIterator;
+    }
+}
